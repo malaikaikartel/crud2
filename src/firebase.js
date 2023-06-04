c9d5405f3fcf6ffdc678a8086ff930c826cb61d0
malaikaikartel <50528583+malaikaikartel@users.noreply.github.com>
2023-06-04 20:34:03 +0700

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import * as firebase from "firebase";
 import "firebase/firestore";
 
 let config = {
-  apiKey: "xxx",
-  authDomain: "bezkoder-firebase.firebaseapp.com",
-  databaseURL: "https://bezkoder-firebase.firebaseio.com",
-  projectId: "bezkoder-firebase",
-  storageBucket: "bezkoder-firebase.appspot.com",
-  messagingSenderId: "xxx",
-  appId: "xxx",
+    apiKey: "AIzaSyBy1fxtGHrXVFlHHYUVafny5SHV8rVCs6c",
+  authDomain: "redframe-77d5a.firebaseapp.com",
+  projectId: "redframe-77d5a",
+  storageBucket: "redframe-77d5a.appspot.com",
+  messagingSenderId: "806740627891",
+  appId: "1:806740627891:web:7025d1386396f9cdb3fbc6",
+  measurementId: "G-HSL5EMRL9W"
 };
 
 firebase.initializeApp(config);
